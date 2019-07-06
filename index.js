006d2f9dee616c518fb96d5832f2fe8f281b7eee
Dapo Momodu <momoduoladapo@gmail.com>
2019-07-07 00:45:15 +0100

    Enter

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+const forms = document.querySelectorAll(".form");
 
-const forms = document.querySelectorAll("button[type=submit]");
 for (const form of forms) {
-    form.addEventListener('click', function (event) {
-        let productName = document.getElementById('productName').innerText;
-        let productImg = document.getElementById('productImg').src;
-        let package = document.getElementById('package').value;
+  form.addEventListener('submit', function(event) {
 
-        alert('Working See Details Below:' + '\n' + productName + '\n\n' + productImg + '\n\n' + package);
-    })
+    event.preventDefault();
+
+    let productName = this.querySelector('.productName').innerText;
+    let productImg = this.querySelector('.productImg').src;
+    let package = this.querySelector('.package').value;
+
+    alert('Working See Details Below:' + '\n' + productName + '\n\n' + productImg + '\n\n' + package);
+
+  });
 }
-
-
-
-
