18535bba3fbfa26d0ab46045a1227abf139368f7
Dapo Momodu <momoduoladapo@gmail.com>
2019-07-06 23:49:21 +0100

    Enter

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 console.log('Application started, button yet to be clicked');
 
 
-const forms = document.querySelectorAll("button[type=submit]")
+const forms = document.querySelectorAll("button[type=submit]");
 for (const form of forms) {
   form.addEventListener('click', function(event) {
     let productName = document.getElementById('productName').innerText;
     let productImg = document.getElementById('productImg').src;
     let package = document.getElementById('package').value;
-    
-    alert('Working See Details Below:' + '\n' + productName + '\n\n' + productImg + '\n\n' + package);
 
-    console.log('Application Ended');
+    alert('Working See Details Below:' + '\n' + productName + '\n\n' + productImg + '\n\n' + package);
   })
 }
+
 
 
 
